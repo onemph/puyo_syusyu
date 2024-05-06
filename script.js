1982f48258febb138fb32bb145768aa0ba91f2cb
onemph <144752020+onemph@users.noreply.github.com>
2024-05-07 08:40:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,10 @@ function calculateAverage() {
 
         output += `${formattedDate} ${runningTotal.toLocaleString()}<br>`;
         
-        if (currentDate.toISOString().split('T')[0] === new Date().toISOString().split('T')[0]) {
+        // JSTで現在の日付を取得
+        const currentDateJST = new Date(new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'})).toISOString().split('T')[0];
+        
+        if (formattedDate === currentDateJST) {
             if (i === 0) {
                 copyText = `今回の完走は${quantity.toLocaleString()}個、日数は${daysDiff+1}日なので、1日の平均個数は\n${dailyAverage.toLocaleString()}個\nです。`;
             } else {
