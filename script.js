6d2e4acc09833d14a881e5e422e82687bd9406e5
onemph <144752020+onemph@users.noreply.github.com>
2024-05-07 08:49:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -52,10 +52,6 @@ function calculateAverage() {
         const currentDateJST = new Date(new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'})).toISOString().split('T')[0];
         
         console.log('new Date: ', new Date());
-        console.log('new Date().toLocaleString', new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'});
-        console.log('new Date().toLocaleString.toISOString()', new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'})).toISOString());
-        console.log('new Date().toLocaleString.toISOString().split', new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'})).toISOString().split('T')[0]);
-        console.log('new Date(new Date().toLocaleString.toISOString().split', new Date(new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'})).toISOString().split('T')[0]);
         
         if (formattedDate === currentDateJST) {
             if (i === 0) {
