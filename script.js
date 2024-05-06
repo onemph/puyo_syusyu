0e61f2b20c2b4914fb6975e43632b6df719c9e74
onemph <144752020+onemph@users.noreply.github.com>
2024-05-06 13:31:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,45 +24,46 @@ function calculateAverage() {
     const endDate = new Date(document.getElementById("end-date").value + "T00:00:00Z");
     const daysDiff = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
 
+    if (!(quantity && startDate && endDate && daysDiff > 0)) {
+        alert("値を正しく入力してください。");
+        return;
+    }
+
     const dailyAverage = Math.ceil(quantity / (daysDiff + 1)); // dailyAverage をループ外で計算
     let copyText = ''; // copyText を宣言
 
-    if (quantity && startDate && endDate && daysDiff > 0) {
-        let output = '';
-        let runningTotal = 0;
+    let output = '';
+    let runningTotal = 0;
 
-        for (let i = 0; i <= daysDiff; i++) {
-            const currentDate = new Date(startDate);
-            currentDate.setDate(startDate.getDate() + i);
-            const formattedDate = currentDate.toISOString().split('T')[0];
+    for (let i = 0; i <= daysDiff; i++) {
+        const currentDate = new Date(startDate);
+        currentDate.setDate(startDate.getDate() + i);
+        const formattedDate = currentDate.toISOString().split('T')[0];
 
-            runningTotal += dailyAverage;
-            // runningTotalがquantityを超えた場合、quantityの値にする
-            if (runningTotal > quantity) {
-                runningTotal = quantity;
-            }
-
-            output += `${formattedDate} ${runningTotal.toLocaleString()}<br>`;
-
-            if (currentDate.toISOString().split('T')[0] === new Date().toISOString().split('T')[0]) {
-                if (i === 0) {
-                    copyText = `今回の完走は${quantity.toLocaleString()}個、日数は${daysDiff+1}日なので、1日の平均個数は\n${dailyAverage.toLocaleString()}個\nです。`;
-                } else {
-                    copyText = `おはようございます。今日の目安は\n${runningTotal.toLocaleString()}個\nです。`;
-                }
+        if (currentDate.toISOString().split('T')[0] === new Date().toISOString().split('T')[0]) {
+            if (i === 0) {
+                copyText = `今回の完走は${quantity.toLocaleString()}個、日数は${daysDiff+1}日なので、1日の平均個数は\n${dailyAverage.toLocaleString()}個\nです。`;
+            } else {
+                copyText = `おはようございます。今日の目安は\n${runningTotal.toLocaleString()}個\nです。`;
             }
         }
 
-        document.getElementById("output").innerHTML = output;
+        runningTotal += dailyAverage;
+        // runningTotalがquantityを超えた場合、quantityの値にする
+        if (runningTotal > quantity) {
+            runningTotal = quantity;
+        }
 
-        // ボタンを押した日のコピーされるテキストを設定
-        navigator.clipboard.writeText(copyText);
-
-        // 値をローカルストレージに保存する
-        localStorage.setItem('quantity', quantity);
-        localStorage.setItem('startDate', document.getElementById("start-date").value);
-        localStorage.setItem('endDate', document.getElementById("end-date").value);
-    } else {
-        alert("値を正しく入力してください。");
+        output += `${formattedDate} ${runningTotal.toLocaleString()}<br>`;
     }
+
+    document.getElementById("output").innerHTML = output;
+
+    // ボタンを押した日のコピーされるテキストを設定
+    navigator.clipboard.writeText(copyText);
+
+    // 値をローカルストレージに保存する
+    localStorage.setItem('quantity', quantity);
+    localStorage.setItem('startDate', document.getElementById("start-date").value);
+    localStorage.setItem('endDate', document.getElementById("end-date").value);
 }
